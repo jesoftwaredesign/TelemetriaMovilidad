3145bb8bc3ad87157de0e58783799e7b08f4d486
proyectoenergetica <62725772+proyectoenergetica@users.noreply.github.com>
2020-05-06 22:26:18 -0500

    make it better

---
+++ src/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env;
+
+//const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
+
+const MONGODB_URI = 'mongodb+srv://next:corozal@cluster0-psf8f.mongodb.net/notas?retryWrites=true&w=majority';
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+  })
+  .then(db => console.log("DB is connected"))
+  .catch(err => console.error(err));
